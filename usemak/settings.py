2bee2b44be9f13491ed0d533cbcf2c16a5614b2b
Carlos Artur <arturmds@yahoo.com.br>
2024-06-07 21:28:46 -0300

    Add db Postgres

--- usemak/settings.py
+++ usemak/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +86,11 @@ DATABASES = {
     }
 }
 
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
