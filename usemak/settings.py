495b423b7122276501a4272658a4aecb87a067a0
Carlos Artur <arturmds@yahoo.com.br>
2025-01-27 17:08:42 -0300

    Corrigindo bug de secret access key aws 2

--- usemak/settings.py
+++ usemak/settings.py
@@ -168,10 +168,12 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-AWS_ACCESS_KEY_ID = "AKIA6ODU5O47ZX4OR6OS"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "heroku-usemak-artursantana"
+if DATABASE_URL:
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
