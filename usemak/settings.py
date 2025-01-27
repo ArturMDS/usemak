4f4b94d1e511e60bb1b3970aadd69997cbc37528
Carlos Artur <arturmds@yahoo.com.br>
2025-01-27 16:39:11 -0300

    Corrigindo bug S3

--- usemak/settings.py
+++ usemak/settings.py
@@ -173,7 +173,6 @@ if DATABASE_URL:
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
